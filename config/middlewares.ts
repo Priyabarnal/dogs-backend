4da124dfab51dc229474bdc4a67c465f94026033
Priyabarnal <priyabarnal09@gmail.com>
2026-02-10 22:05:37 +0530

    updated

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,10 +1,12 @@
-module.exports = [
-  {
-    name: "strapi::cors",
-    config: {
-      origin: ["http://localhost:3000"],
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      headers: ["Content-Type", "Authorization"],
-    },
-  },
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
